6dcb662e0c75b688ba3fef94ca8eaee6d07352c5
John Spencer <johnspencer15@gmail.com>
2017-04-05 12:00:17 -0400

    print data on server

--- data_ingest/data_ingest.py
+++ data_ingest/data_ingest.py
@@ -8,7 +8,11 @@ def hello():
 
 @application.route("/data", methods=["POST"])
 def data_ingest():
-    print(request.get_json()['x_data'])
+    print('X DATA', request.get_json()['x_data'][0:10])
+    print('Y DATA', request.get_json()['y_data'][0:10])
+    print('Z DATA', request.get_json()['z_data'][0:10])
+    # print('LAT DATA', request.get_json()['lat_data'][0:10])
+    # print('LONG DATA', request.get_json()['long_data'][0:10])
     return jsonify(result="success")
 
 @application.route("/get_data", methods=["GET"])

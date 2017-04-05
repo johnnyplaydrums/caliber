08e40d66df075de4b052abaa3e2722cc6365826c
John Spencer <johnspencer15@gmail.com>
2017-04-05 03:21:41 -0400

    interval data calls

--- data_ingest/static/javascript/get_data.js
+++ data_ingest/static/javascript/get_data.js
@@ -1,14 +1,17 @@
 $(function() {
-    $.ajax({
-      type: 'get',
-      url: 'http://34.205.150.122/get_data',
-      dataType: "json"
-    })
-    .done(function(response) {
-      console.log(response);
-      $("#data").append(response);
-    })
-    .always(function() {
-      console.log("data received");
-    });
+    setInterval(function() {
+      $.ajax({
+        type: 'get',
+        url: 'http://34.205.150.122/get_data',
+        dataType: "json"
+      })
+      .done(function(response) {
+        console.log(response);
+        $("#data").append(response);
+        $("#data").append('<br>');
+      })
+      .always(function() {
+        console.log("data received");
+      });
+    }, 5000);
 });

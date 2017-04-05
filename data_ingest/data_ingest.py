8b56b6942b18fda5dee7bc4b48a2fad1808a8e5b
John Spencer <johnspencer15@gmail.com>
2017-04-05 02:37:45 -0400

    retreive data from front end

--- data_ingest/data_ingest.py
+++ data_ingest/data_ingest.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 application = Flask(__name__)
+application.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+x_data = [];
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template('index.html')
+
+@application.route("/data", methods=["POST"])
+def data_ingest():
+    print(request.get_json()['x_data'])
+    x_data = request.get_json()['x_data']
+    return jsonify(result="success")
+
+@application.route("/get_data", methods=["GET"])
+def get_data():
+    print('get_data')
+    return jsonify(x_data=x_data)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')

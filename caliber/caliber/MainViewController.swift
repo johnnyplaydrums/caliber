a6aedcc0096a6302f00688037ec4b817e603bff7
John Spencer <johnspencer15@gmail.com>
2017-04-05 02:36:51 -0400

    Add json calls

--- caliber/caliber/MainViewController.swift
+++ caliber/caliber/MainViewController.swift
@@ -27,7 +27,9 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var longitude: UILabel!
     var global_lat = ""
     var global_long = ""
-    
+    var x_array: [Double] = []
+    var y_array: [Double] = []
+    var z_array: [Double] = []
     
     override func viewDidLoad()
     {
@@ -44,7 +46,7 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         
         Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(MainViewController.gyro_update), userInfo: nil, repeats: true)
         
-        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(MainViewController.post_data), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(MainViewController.post_data), userInfo: nil, repeats: true)
 
     }
 
@@ -62,42 +64,66 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         
         if let x_data = CMmanager.gyroData?.rotationRate.x {
             x_label.text = String(x_data)
+            x_array.append(x_data)
         }
         if let y_data = CMmanager.gyroData?.rotationRate.y {
             y_label.text = String(y_data)
+            y_array.append(y_data)
         }
         if let z_data = CMmanager.gyroData?.rotationRate.z {
             z_label.text = String(z_data)
+            z_array.append(z_data)
         }
         
     }
     
     func post_data(){
         print("POST CALLED")
-        let url = URL(string: "http://google.com")!
+        let url = URL(string: "http://34.205.150.122/data")!
         var request = URLRequest(url: url)
-    
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        let jsonObject: [String: [Double]]  = [ "x_data": x_array, "y_data": y_array, "z_data": z_array ]
+        let jsonData = try! JSONSerialization.data(withJSONObject: jsonObject, options: [])
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
         
-            let jsonDict: [String: AnyObject] = ["test": 1 as AnyObject]
-            let jsonData = try! JSONSerialization.data(withJSONObject: jsonDict, options: [])
+        print(x_array)
+        print(x_array.count)
+        x_array.removeAll()
 
-            request.httpMethod = "get"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.httpBody = jsonData
-        
-            if let error = error {
-                print("error:", error)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
                 return
             }
             
-            do {
-//             let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                let string = String(describing: data)
-                print(string)
-            } catch {
-                print("error2:", error)
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
             }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        
+            let jsonDict: [String: AnyObject] = ["test": 1 as AnyObject]
+            let jsonData = try! JSONSerialization.data(withJSONObject: jsonDict, options: [])
+//
+//            request.httpMethod = "POST"
+//            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//            request.httpBody = jsonData
+//        
+//            if let error = error {
+//                print("error:", error)
+//                return
+//            }
+//            
+//            do {
+////             let json = try JSONSerialization.jsonObject(with: data!, options: [])
+//                let string = String(describing: data)
+//                print(string)
+//            } catch {
+//                print("error2:", error)
+//            }
         }
         task.resume()
     }

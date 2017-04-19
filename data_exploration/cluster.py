234b54cc2a02588c657b9a25536a8b42e87f1aae
bytesizedroll <damienhobday@gmail.com>
2017-04-19 12:07:01 -0400

    Wrapped in functions.

--- data_exploration/cluster.py
+++ data_exploration/cluster.py
@@ -56,11 +56,15 @@ def get_jenks_breaks(data_list, number_class):
 
 x = [27.363046916666665, 19.114167833333333, 28.167875083333335, 16.843899333333336, 33.62408525, 38.42577816666667, 47.04649625, 37.51921658333333, 14.684923999999999, 7.169020416666666, 8.518746499999999, 11.093779583333333, 6.229961166666666, 5.671418749999999, 8.607262166666668, 6.142840833333333, 7.787132166666666, 9.109575333333332, 6.434557749999999, 9.747043, 9.430098, 7.193873916666666, 11.650239416666667, 7.565074916666667, 6.654198416666666]
 
-breaks = get_jenks_breaks(x, 3)
+def classify(data):
+    tuples
+    breaks = get_jenks_breaks(data, 3)
 
-for line in breaks:
-    plt.plot([line for _ in range(len(x))], 'k--')
+    for line in breaks:
+        plt.plot([line for _ in range(len(data))], 'k--')
 
-plt.plot(x, linestyle="", marker="o")
-plt.grid(True)
-plt.show()
+    plt.plot(data, linestyle="", marker="o")
+    plt.grid(True)
+    plt.show()
+
+classify(x)

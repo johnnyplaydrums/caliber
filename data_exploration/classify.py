6e150adae9306457fedce1108baa2fdf0d65508d
bytesizedroll <damienhobday@gmail.com>
2017-04-19 13:45:15 -0400

    Changed file names to make more sense. Added analyze.py which will compile all parts of data processing in one script.

--- data_exploration/classify.py
+++ data_exploration/classify.py
@@ -53,9 +53,6 @@ def get_jenks_breaks(data_list, number_class):
         count_num -= 1
     return kclass
 
-
-x = [27.363046916666665, 19.114167833333333, 28.167875083333335, 16.843899333333336, 33.62408525, 38.42577816666667, 47.04649625, 37.51921658333333, 14.684923999999999, 7.169020416666666, 8.518746499999999, 11.093779583333333, 6.229961166666666, 5.671418749999999, 8.607262166666668, 6.142840833333333, 7.787132166666666, 9.109575333333332, 6.434557749999999, 9.747043, 9.430098, 7.193873916666666, 11.650239416666667, 7.565074916666667, 6.654198416666666]
-
 def classify_graph(data):
     tuples
     breaks = get_jenks_breaks(data, 3)          
@@ -70,7 +67,7 @@ def classify_graph(data):
 def classify(data):
     breaks = get_jenks_breaks(data, 3)
     all_tuples = []
-    print(breaks)
+    #print(breaks)
 
     for num in data:
         if num >= breaks[0] and num <= breaks[1]:
@@ -85,13 +82,7 @@ def classify(data):
             my_tuple = (num , 'Bad')
             all_tuples.append(my_tuple)
             #print(my_tuple)
-    print(all_tuples)
+    return all_tuples
         
-    for line in breaks:
-        plt.plot([line for _ in range(len(data))], 'k--')
 
-    plt.plot(data, linestyle="", marker="o")
-    plt.grid(True)
-    plt.show()
 
-classify(x)

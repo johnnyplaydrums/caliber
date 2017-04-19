28c3320760ea96599f99d83dee07c141a1894ce8
bytesizedroll <damienhobday@gmail.com>
2017-04-19 11:01:50 -0400

    Taking integrals, plotting.

--- data_exploration/poly_int.py
+++ data_exploration/poly_int.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.integrate import simps
+from matplotlib.backends.backend_pdf import PdfPages
 
-df = pd.read_csv('gyro_x.csv')
+pp = PdfPages('multipage.pdf')
+chunksize = 20
+total_area = []
+count = 0
 
-x = df['Time'].as_matrix()
-y = df['X'].as_matrix()
+for df in pd.read_csv('gyro_x.csv', chunksize=chunksize, iterator=True):
+	
+	x = df['Time'].as_matrix()
+	y = df['X'].as_matrix()
 
-plt.plot(x, y)
-plt.show()
+	area = simps(y)
+	total_area.append(abs(area))
+	plt.plot(x, y)
+	pp.savefig()
+	plt.clf()
+	count += 1
 
-print(x)
 
-print(y)
+pp.close()
+print(total_area)

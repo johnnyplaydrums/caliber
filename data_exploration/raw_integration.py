305abdf4dbd03e917ccbf0545ba09fc56545bd09
bytesizedroll <damienhobday@gmail.com>
2017-04-20 14:22:28 -0400

    More condensing, added more csv data.

--- data_exploration/raw_integration.py
+++ data_exploration/raw_integration.py
@@ -3,17 +3,19 @@ import matplotlib.pyplot as plt
 from scipy.integrate import simps
 from matplotlib.backends.backend_pdf import PdfPages
 
-pp = PdfPages('multipage.pdf')
+"""pp = PdfPages('multipage.pdf')
 chunksize = 20
 total_area = []
 count = 0
 
 for df in pd.read_csv('gyro_x.csv', chunksize=chunksize, iterator=True):
-	
+
 	x = df['Time'].as_matrix()
 	y = df['X'].as_matrix()
 
 	area = simps(y)
+	print(area)
+	print(count)
 	total_area.append(abs(area))
 	plt.plot(x, y)
 	pp.savefig()
@@ -22,4 +24,19 @@ for df in pd.read_csv('gyro_x.csv', chunksize=chunksize, iterator=True):
 
 
 pp.close()
-print(total_area)
+print(total_area)"""
+
+def integrate(filename):
+    chunksize = 20
+    total_area = []
+    count = 0
+
+    for df in pd.read_csv('gyro_x.csv', chunksize=chunksize, iterator=True):
+
+	    x = df['Time'].as_matrix()
+	    y = df['X'].as_matrix()
+	    area = simps(y)
+	    total_area.append(abs(area))
+	    count += 1
+
+    return total_area

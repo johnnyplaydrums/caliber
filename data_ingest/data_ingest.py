461d63d3a6c9bb29205d1c0880e0a89cb8f23eca
John Spencer <johnspencer15@gmail.com>
2017-04-24 16:07:27 -0400

    pull process_data into separate module

--- data_ingest/data_ingest.py
+++ data_ingest/data_ingest.py
@@ -1,7 +1,5 @@
-import geocoder, math
-from datetime import datetime
 from flask import Flask, render_template, request, jsonify
-from views.send_to_dynamodb import send_to_dynamodb
+from utils.process_data import process_data
 
 application = Flask(__name__)
 application.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -13,43 +11,7 @@ def hello():
 @application.route("/data", methods=["POST"])
 def data_ingest():
     data = request.get_json()
-    address_points = {}
-
-    last_point = (0,0)
-    for index in range(len(data['lat'])):
-        current_point = (data['lat'][index], data['long'][index])
-        if last_point != current_point:
-            print(last_point, current_point)
-            address = geocoder.google([current_point[0], current_point[1]], method='reverse')
-            last_address = str(int(math.ceil(int(address.address.split(' ', 1)[0]) / 100) * 100)) + ' ' + address.address.split(' ', 1)[1]
-            if last_address in address_points:
-                address_points[last_address]['lat'].append(current_point[0])
-                address_points[last_address]['long'].append(current_point[1])
-                address_points[last_address]['x'].append(data['x'][index])
-                address_points[last_address]['y'].append(data['y'][index])
-                address_points[last_address]['z'].append(data['z'][index])
-            else:
-                address_points[last_address] = {
-                    'lat': [current_point[0]],
-                    'long': [current_point[1]],
-                    'x': [data['x'][index]],
-                    'y': [data['y'][index]],
-                    'z': [data['z'][index]],
-                    'datetime': datetime.now().strftime('%H:%M:%S %B %d %Y'),
-                    'address': last_address,
-                    'processed': 'false'
-                }
-
-            last_point = current_point
-            print(last_address)
-        else:
-            address_points[last_address]['lat'].append(current_point[0])
-            address_points[last_address]['long'].append(current_point[1])
-            address_points[last_address]['x'].append(data['x'][index])
-            address_points[last_address]['y'].append(data['y'][index])
-            address_points[last_address]['z'].append(data['z'][index])
-
-    send_to_dynamodb(address_points)
+    process_data(data)
     return jsonify(result="success")
 
 @application.route("/get_data", methods=["GET"])
